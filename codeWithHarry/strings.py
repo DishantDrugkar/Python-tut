c6d97586f667590bf71f841eb823818de07c64ca
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-12-11 13:07:27 +0530

    String

---
+++ codeWithHarry/strings.py
@@ -0,0 +1,19 @@
+s = "Hello"
+
+print(len(s))
+print(s[0:3])
+print(s[ : 4])
+
+str = "hello dishant"
+
+print(str.capitalize())
+print(str.upper())
+print(str.title())
+print(str.replace("dishant", "Rohini"))
+print(str.count("l"))
+print(f"Hello My Name is {s} and my username is {str}")
+
+
+
+name = input("Enter Your Name : ")
+print(f"Good Afternoon {name}")
